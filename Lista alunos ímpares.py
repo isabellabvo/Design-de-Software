bc2f047ccbd91ae515c55995e27a460a74f560c4
isabellabvo <63018304+isabellabvo@users.noreply.github.com>
2020-06-24 15:31:48 -0300

    Create Lista alunos ímpares.py

---
+++ Lista alunos ímpares.py
@@ -0,0 +1,13 @@
+#---------ENUNCIADO---------#
+
+'''
+Escreva uma função que recebe uma lista de nomes de alunos e devolve uma lista contendo somente os alunos nos índices ímpares, dividindo a turma em dois.
+
+O nome da sua função deve ser alunos_impares.
+'''
+
+#----------CÓDIGO-----------#
+
+def alunos_impares(lista):
+    k = lista[1::2]
+    return k

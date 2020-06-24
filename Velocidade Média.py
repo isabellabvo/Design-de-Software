e88e82f86be20957b948e257fe61cf10a53041ed
isabellabvo <63018304+isabellabvo@users.noreply.github.com>
2020-06-24 00:33:53 -0300

    Update Velocidade Média.py

--- Velocidade Média.py
+++ Velocidade Média.py
@@ -1,9 +1,9 @@
 #-------------ENUNCIADO-------------#
-
+'''
 Escreva uma função que recebe um valor de distância, em km, percorrida por um veículo e o tempo gasto, em horas, para percorrer esta distância. 
 A função retorna a velocidade média do veículo em km/h.
 O nome da sua função deve ser calcula_velocidade_media.
-
+'''
 #--------------CÓDIGO---------------#
 
 def calcula_velocidade_media (valor_da_distancia, tempo_gasto):

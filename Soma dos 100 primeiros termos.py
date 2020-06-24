f84deb4912f59b0a650c059b558543108228f64f
isabellabvo <63018304+isabellabvo@users.noreply.github.com>
2020-06-24 01:13:28 -0300

    Create Soma dos 100 primeiros termos.py

---
+++ Soma dos 100 primeiros termos.py
@@ -0,0 +1,19 @@
+#---------ENUNCIADO---------#
+
+'''
+Some os 100 primeiros termos da expressão abaixo usando while e imprima o resultado:
+
+'''
+
+#----------CÓDIGO-----------#
+soma=0
+for i in range (100):
+    soma+= 1/2**i
+    print (soma)
+    
+#x=0
+#k=0
+#while x < 100:
+    #k += 1/(2)**x
+    #x+=1
+#print (k)

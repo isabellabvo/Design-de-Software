df2aecfac1d5d93f716bf238489bb84189d9ab8e
isabellabvo <63018304+isabellabvo@users.noreply.github.com>
2020-06-24 13:17:42 -0300

    Create Repete asteriscos.py

---
+++ Repete asteriscos.py
@@ -0,0 +1,12 @@
+#---------ENUNCIADO---------#
+
+'''
+Faça uma função que recebe um inteiro positivo n e retorna uma string contendo uma sequência de n asteriscos.
+
+O nome da sua função deve ser asteriscos.
+'''
+
+#----------CÓDIGO-----------#
+
+def asteriscos (n):
+    return "*"*n

e5e52e4c2133a58b26c8cf1494ca1811a6530b29
isabellabvo <63018304+isabellabvo@users.noreply.github.com>
2020-06-24 00:33:23 -0300

    Update Juros Compostos.py

--- Juros Compostos.py
+++ Juros Compostos.py
@@ -1,9 +1,10 @@
 #----------------ENUNCIADO----------------#
+'''
 Escreva uma função que calcula o valor devido ao final de um empréstimo. 
 Os argumentos de entrada da função serão: valor emprestado, número de meses e taxa de juros. 
 Atenção: usar juros compostos para calcular o valor devido.
 O nome da sua função deve ser calcula_valor_devido.
-
+'''
 #-----------------CÓDIGO------------------#
 
 def calcula_valor_devido (valor_emprestado, número_de_meses, taxa_de_juros):

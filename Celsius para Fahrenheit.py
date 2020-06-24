9971644b146eccbb9e56cc972cd644aacdbef1b2
isabellabvo <63018304+isabellabvo@users.noreply.github.com>
2020-06-24 00:41:56 -0300

    Create Celsius para Fahrenheit.py

---
+++ Celsius para Fahrenheit.py
@@ -0,0 +1,15 @@
+#-----------ENUNCIADO-----------#
+'''
+Faça uma função que converte temperaturas em Celsius para Fahrenheit.
+
+O nome da sua função deve ser celsius_para_fahrenheit.
+'''
+
+#------------CÓDIGO-------------#
+
+def libras_para_kg (libras):
+    kg = libras * 0.453592
+    return kg
+a = 100
+kilograma = libras_para_kg (a)
+print (kilograma)
